ec4562971ca1415cfb58c02fdcfed8fd7f69f647
Emeka Osuagwu <emekaosuagwuandela@gmail.com>
2019-02-13 19:34:17 +0100

    setup EventEmitter class

--- src/EventEmitter.php
+++ src/EventEmitter.php
@@ -15,4 +15,20 @@ class EventEmitter implements EventEmitterInterface
 		# code...
 	}
 
+	public function on(string $event, callable $listener)
+	{
+	}
+
+	public function removeListener(string $event, callable $listener){
+		
+	}
+
+	public function removeAllListeners(string $event = null){
+
+	}
+
+	public function emit(string $event, ...$arguments){
+
+	}
+
 }
